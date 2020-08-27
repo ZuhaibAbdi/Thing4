ca190c0ea5955450c1c79fe75780d5f058c33caa
Coding <coding@Zakarias-MacBook-Pro.local>
2020-08-27 11:32:14 -0600

    ,m

---
+++ script.js
@@ -0,0 +1,24 @@
+/* 
+=================================
+* SIGN UP
+=================================
+*/
+
+let form = document.querySelector('.form')
+let name = document.querySelector('#name')
+let email = document.querySelector('#email')
+let nameValue = [];
+let emailValue = [];
+let user;
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    nameValue.unshift(name.value)
+    emailValue.unshift(email.value)
+
+    user = `${nameValue[0]} ${emailValue[0]}`
+    alert(user)
+
+})
